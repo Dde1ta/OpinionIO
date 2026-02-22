004c3cea3a2ac4e98c1f1a12976bbd5fc780b842
Kushagra-Chakraborty <kushagrachakraborty@gmail.com>
2026-02-22 17:38:31 +0530

    wiring together

--- Workers/Processor/consumer.py
+++ Workers/Processor/consumer.py
@@ -1,7 +1,11 @@
 from General.kafkaContracts import *
 from General.kafkaHelper import kafka_app
+from Workers.Processor.logic import preprocess_task_data
+from Workers.Processor.producer import push_task_data
 
 
 @kafka_app.consumes(topic="META_DATA")
 async def next_task(msg: MetaDataContract) -> MetaDataContract:
+    task_data = preprocess_task_data(msg)
+    await push_task_data(task_data)
     return msg
